a30ad76872219a97914afdc22e599cbe0fffe237
Fernando_Braz <f.braz@volcanic.co.uk>
2013-08-27 17:26:36 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetrack::Application.config.secret_token = '84703e3e3f90847762ca5fb2f568b539dc230c443952e76d05e83ff884acf94def3ea12813802649d586ca4b31b79ac24204266d5f0b0a343f63c6a19e2d276c'
